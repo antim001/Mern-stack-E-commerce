7a724546dcc3d0e703fb94d706a34104c7d79160
Antim <antimsarker0@gmail.com>
2024-06-06 01:01:47 +0600

    basic setup

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express')
+import express  from 'express'
+import dotenv from 'dotenv'
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000;
+dotenv.config()
 
 app.get('/', (req, res) => {
-  res.send('welcome to e-commerce')
+  res.send('<h1>Welcome to my e-commerce</h1>')
 })
 
 app.listen(port, () => {
